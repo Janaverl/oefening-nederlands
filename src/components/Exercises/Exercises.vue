1ef26252617d4c2ffdd279cca92170c3702e93a1
Janaverl <36476740+Janaverl@users.noreply.github.com>
2020-06-06 18:01:24 +0200

    Update: randomize exercises

--- src/components/Exercises/Exercises.vue
+++ src/components/Exercises/Exercises.vue
@@ -72,9 +72,18 @@
 
     export default {
         name: 'Exercises',
-        data: function() {
+          data: function() {
+            function shuffleArray(array) {
+              for (var i = array.length - 1; i > 0; i--) {
+                  var j = Math.floor(Math.random() * (i + 1));
+                  var temp = array[i];
+                  array[i] = array[j];
+                  array[j] = temp;
+              }
+            return array;
+          }
           return {
-            exercises: jsonExercises,
+            exercises: shuffleArray(jsonExercises),
             progress: {
               i: 0,
               current: 1,
