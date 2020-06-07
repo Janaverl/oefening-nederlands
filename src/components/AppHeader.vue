37cbc69cc722af05525bc2012951df7cb887bebb
Janaverl <36476740+Janaverl@users.noreply.github.com>
2020-06-07 18:01:14 +0200

    Add: menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
     <nav
         class="navbar navbar-light bg-light mb-5"
     >
@@ -10,11 +11,23 @@
 
         <span
             class="navbar-text"
-            @click="openScoreBoard"
+            @click="showMenu = !showMenu"
         >
-            Bekijk mijn scorebord
+            Menu
         </span>
     </nav>
+    <div v-if="showMenu"
+        class="menu"
+        @click="showMenu = !showMenu"
+    >
+        <p
+            @click="openScoreBoard()"
+        >alle scores</p>
+        <p
+            @click="startExcercise()"
+        >nieuwe oefening starten</p>
+    </div>
+</div>
 </template>
 
 <script>
@@ -22,17 +35,25 @@
 
     export default {
         name: 'AppHeader',
+        data: function() {
+            return {
+                showMenu: false
+            };
+        },
         props: {
             appTitle: String
         },
         methods: {
             openScoreBoard() {
                 ContentEventBus.$emit('openScoreBoard')
-            }
+            },
+            startExcercise() {
+                ContentEventBus.$emit('reStartExercises');
+            },
         }
 
     }
 </script>
 
-<style>
+<style scoped>
 </style>
